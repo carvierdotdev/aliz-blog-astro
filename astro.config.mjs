39f7bdd6dd6e9a4b04d23a1c578641ebb9153063
Carvier <60159324+carvierdotdev@users.noreply.github.com>
2024-08-03 10:16:40 -0400

    feat: add RSS feed generation
    - Added site URL to `astro.config.mjs`
    - Created `rss.xml.js` to generate an RSS feed for the blog posts.
    
    Part of Astro Tutorial
    
    commit-id:8ad07549

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://aliz-blog-astro.vercel.app/",
   output: "server",
   adapter: vercel({
     webAnalytics: { enabled: true },
