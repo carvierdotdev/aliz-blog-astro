ed34ec18aa368afcd66a95a111745c885edbf81e
Carvier <60159324+carvierdotdev@users.noreply.github.com>
2024-08-21 12:02:17 -0400

    feat(content): add content collection schema
    •  Add `src/content/config.ts` to define a content collection schema
    
    •  Use `astro:content` to define the collection and schema
    
    •  Include fields for title, pubDate, description, author, image, and tags
    
    commit-id:4ef89c56

---
+++ src/content/config.ts
@@ -0,0 +1,22 @@
+// To learn about it: [Content Collection - Defining schema](https://docs.astro.build/en/guides/content-collections/#defining-a-collection-schema)
+
+import { defineCollection, z } from "astro:content";
+
+const postCollection = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    pubDate: z.date(),
+    description: z.string(),
+    author: z.string(),
+    image: z.object({
+      url: z.string(),
+      alt: z.string(),
+    }),
+    tags: z.array(z.string()),
+  }),
+});
+
+export const collections = {
+  posts: postCollection,
+};
